86812fb7c8e3fd13c12771758bfdaabcaafe2342
sky-aman <aman.dubey.sky@gmail.com>
2024-01-04 15:47:28 +0530

    add to cart added

--- src/app/add-product/page.tsx
+++ src/app/add-product/page.tsx
@@ -1,7 +1,62 @@
+import { Metadata } from 'next';
+import { redirect } from 'next/navigation';
+import FormSubmitButton from '@/component/FormSubmitButton';
+import prisma from '@/lib/db/prisma';
+
+export const metadata: Metadata = {
+  title: 'Add Product | We-commerce',
+};
+
+async function addProduct(formData: FormData) {
+  'use server';
+  const name = formData.get('name')?.toString();
+  const description = formData.get('description')?.toString();
+  const imageUrl = formData.get('imageUrl')?.toString();
+  const price = Number(formData.get('price')) ?? 0;
+
+  if (!name || !description || !imageUrl || !price) {
+    throw Error('Missing required fields');
+  }
+
+  await prisma?.product.create({
+    data: { name, description, imageUrl, price },
+  });
+
+  redirect('/');
+}
+
 export default function AddProductPage() {
-    return (
-        <div>
-            <h1 className='text-lg mb-3 font-bold'>Add Product</h1>
-        </div>
-    )
+  return (
+    <div>
+      <h1 className='mb-3 text-lg font-bold'>Add Product</h1>
+      <form action={addProduct}>
+        <input
+          name='name'
+          placeholder='Name'
+          className='input input-bordered mb-3 w-full'
+        />
+        <textarea
+          required
+          name='description'
+          placeholder='Description'
+          className='textarea textarea-bordered mb-3 w-full'
+        />
+        <input
+          name='imageUrl'
+          placeholder='Image Url'
+          type='url'
+          className='input input-bordered mb-3 w-full'
+        />
+        <input
+          name='price'
+          placeholder='Price'
+          type='number'
+          className='input input-bordered mb-3 w-full'
+        />
+        <FormSubmitButton type='submit' className='btn-block'>
+          Add Product
+        </FormSubmitButton>
+      </form>
+    </div>
+  );
 }
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,6 +1,4 @@
-import type { Config } from 'tailwindcss';
-
-const config: Config = {
+module.exports = {
   content: [
     './src/pages/**/*.{js,ts,jsx,tsx,mdx}',
     './src/components/**/*.{js,ts,jsx,tsx,mdx}',
@@ -11,22 +9,20 @@ const config: Config = {
     themes: [
       {
         lightTheme: {
-          primary: "#f4aa3a",
-          secondary: "#f4f4a1",
-          accent: "#1be885",
-          neutral: "#272136",
-          "base-100": "#ffffff",
-          info: "#778ad4",
-          success: "#23b893",
-          warning: "#f79926",
-          error: "#ea535a",
+          primary: '#f4aa3a',
+          secondary: '#f4f4a1',
+          accent: '#1be885',
+          neutral: '#272136',
+          'base-100': '#ffffff',
+          info: '#778ad4',
+          success: '#23b893',
+          warning: '#f79926',
+          error: '#ea535a',
           body: {
-            "background-color": "#e3e6e6",
+            'background-color': '#e3e6e6',
           },
         },
       },
     ],
   },
 };
-export default config;
-
